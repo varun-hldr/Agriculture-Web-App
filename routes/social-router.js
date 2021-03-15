73493bfa60c51cf145dc508a44440f403d1e5ab4
haldharvarun <haldharvarun@yahoo.com>
2021-03-15 23:55:27 +0530

    new

--- routes/social-router.js
+++ routes/social-router.js
@@ -13,19 +13,18 @@ module.exports = (router) => {
 
   // when login is successful, retrieve user info
   router.get("/api/login/success", (req, res) => {
-    console.log("backend success");
-    // if (req.user) {
-    //   //   Create and assign a token
-    //   const token = jwt.sign({ user: req.user }, keys.token.TOKEN_SECRET);
-    //   const user = {
-    //     token: token,
-    //     user: req.user,
-    //     success: true,
-    //   };
-    //   res.send(user);
-    // }
-
-    res.send({ user: req.user, success: true, token: "54444445" });
+    if (req.user) {
+      //   Create and assign a token
+      const token = jwt.sign({ user: req.user }, keys.token.TOKEN_SECRET);
+      const user = {
+        token: token,
+        user: req.user,
+        success: true,
+      };
+      res.send(user);
+    } else {
+      res.send({ user: null, success: false, token: "54444445" });
+    }
   });
 
   // when login failed, send failed msg
