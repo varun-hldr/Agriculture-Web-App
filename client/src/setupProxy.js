d6b7f7a1b8c90e4eb3a8e8bcc2aa33766237d6b2
haldharvarun <haldharvarun@yahoo.com>
2021-03-15 21:18:05 +0530

    local proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require("http-proxy-middleware");
 
-const target = "https://agribazzar.herokuapp.com/";
-// const target = "http://localhost:3100/";
+// const target = "https://agribazzar.herokuapp.com/";
+const target = "http://localhost:3100/";
 
 module.exports = function (app) {
   app.use(proxy("/auth/google", { target: target }));
