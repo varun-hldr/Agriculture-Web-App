f3f16d3e0abcdfc8bbb2359fac096b8931a1975f
haldharvarun <haldharvarun@yahoo.com>
2021-03-15 16:21:46 +0530

    New Update

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require("http-proxy-middleware");
 // module.exports = function (app) {
 //   app.use(proxy("/auth/google", { target: "http://localhost:3100/" }));
 //   app.use(proxy("/auth/facebook", { target: "http://localhost:3100/" }));
-//   app.use(proxy("/*", { target: "http://localhost:3100/" }));
+//   //   app.use(proxy("/api/*", { target: "http://localhost:3100/" }));
 // };
 module.exports = function (app) {
   app.use(
@@ -12,5 +12,5 @@ module.exports = function (app) {
   app.use(
     proxy("/auth/facebook", { target: "https://agribazzar.herokuapp.com/" })
   );
-  app.use(proxy("/*", { target: "https://agribazzar.herokuapp.com/" }));
+  //   app.use(proxy("/*", { target: "https://agribazzar.herokuapp.com/" }));
 };
